70492855f1ccd27e6949542f2327512dfd60b18b
Sadidadri <porerotote7@gmail.com>
2023-07-28 22:52:59 +0200

    update

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    playerLifes = 5;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
