7a97ea4bb136ab0dbc282bdb2544015fb755e0b0
Gekko <pablo.bugarin.camara@gmail.com>
2023-07-29 00:26:11 +0200

    El enemigo sufre daño, nivel básico

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] int experience = 10;
+    // TODO : Por definir [SerializeField] int damage = 10;
+    [SerializeField] int hitPoints = 10;
 
-    private void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Space)) OnDeath();
+        if (collision.tag == "bullet") getDamaged(1); // TODO : Tiene que reconocer el tipo de "arma" y recoger el valor de daño
     }
+
+    void getDamaged(int damage)
+    {
+        hitPoints -= damage;
+        if(hitPoints <= 0) OnDeath();
+    }
+
 
     private void OnDeath()
     {
         if (experience > 0)
             LevelSystem.instance.AddExp(experience);
+        gameObject.SetActive(false);
     }
 }
