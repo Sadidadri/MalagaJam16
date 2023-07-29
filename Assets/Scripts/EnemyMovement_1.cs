281665e2db85ff15accd208d734e6210a932f661
Sadidadri <porerotote7@gmail.com>
2023-07-29 13:37:19 +0200

    Pooling Enemy Spawn 1

--- Assets/Scripts/EnemyMovement_1.cs
+++ Assets/Scripts/EnemyMovement_1.cs
@@ -36,7 +36,7 @@ public class EnemyMovement : MonoBehaviour
         if (transform.position == destinationPosition)
         {
             isMoving = false;
-            enemySpriteRenderer.enabled = false;
+            gameObject.SetActive(false);
             
         }
     }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,20 +5,25 @@ using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
-
+    private static int DEFAULT_HIT_POINTS = 3;
     public Text scoreTxT;
     [SerializeField] int enemy_defeated_xp = 10;
     // TODO : Por definir [SerializeField] int damage = 10;
-    [SerializeField] int hitPoints = 3;
+    [SerializeField] int hitPoints = DEFAULT_HIT_POINTS;
 
     public SpriteRenderer enemySpriteRenderer; // Reference to the enemy's SpriteRenderer component.
     private int numberOfBlinks = 4;
 
-    void Start()
+    void OnEnable()
     {
-
          // Assign the SpriteRenderer component of the enemy GameObject to enemySpriteRenderer.
         enemySpriteRenderer = GetComponent<SpriteRenderer>();
+        enemySpriteRenderer.enabled = true;
+        hitPoints = DEFAULT_HIT_POINTS;
+    }
+
+    void OnDisable(){
+        StopCoroutine(BlinkSprite());
     }
 
    
@@ -32,7 +37,7 @@ public class EnemyScript : MonoBehaviour
         hitPoints -= damage;
         StartCoroutine(BlinkSprite());
         if(hitPoints <= 0) OnDeath();
-        //Hit animation
+    
     }
 
 
@@ -49,7 +54,6 @@ public class EnemyScript : MonoBehaviour
     private IEnumerator BlinkSprite()
     {
         // Store the original visibility state of the sprite before blinking.
-        bool originalVisibility = enemySpriteRenderer.enabled;
         float blinkRate = 0.1f;
 
         // Perform the blinking effect for the duration of the invulnerability time.
@@ -63,7 +67,7 @@ public class EnemyScript : MonoBehaviour
         }
 
         // Ensure that the player's sprite is visible at the end of the blinking effect.
-        enemySpriteRenderer.enabled = originalVisibility;
+        enemySpriteRenderer.enabled = true;
 
     }
 }
