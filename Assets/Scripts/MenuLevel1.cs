101c6756ee26b8dbb016ec27b01ca2a2efbdafba
Gekko <pablo.bugarin.camara@gmail.com>
2023-07-30 12:24:17 +0200

    Cosas

---
+++ Assets/Scripts/MenuLevel1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuLevel1 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menuOpciones, menuGameplay;
+    [SerializeField]
+    Slider volumeSlider;
+    [SerializeField]
+    AudioMixer volumeMixer;
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Level1" && Input.GetKeyDown(KeyCode.Escape)) OpenOptions();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public bool OpenOptions()
+    {
+        if(menuGameplay != null && menuOpciones != null && SceneManager.GetActiveScene().name == "Level1")
+        {
+            menuGameplay.SetActive(false);
+            menuOpciones.SetActive(true);
+            Time.timeScale = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ClosePause()
+    {
+            menuOpciones.SetActive(false); 
+            menuGameplay.SetActive(true);
+
+            Time.timeScale = 1;
+    }
+
+    public void GotoMainMenu() {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+    public void SetVolume()
+    {
+        volumeMixer.SetFloat("MusicVol", Mathf.Log10(volumeSlider.value) * 20);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
