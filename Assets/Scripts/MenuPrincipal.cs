1bbb3e62598f625410f3891e9a70cf4f3e3c5992
Gekko <pablo.bugarin.camara@gmail.com>
2023-07-30 11:45:41 +0200

    Lógica de los menuses

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class MenuPrincipal : MonoBehaviour
 {
     [SerializeField]
-    GameObject menuPrincipal, menuOpciones;
+    GameObject menuPrincipal, menuOpciones, menuGameplay;
     [SerializeField]
     Slider volumeSlider;
     [SerializeField]
@@ -19,10 +19,18 @@ public class MenuPrincipal : MonoBehaviour
         OpenMainMenu();
     }
 
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Level1" && Input.GetKeyDown(KeyCode.Escape)) OpenOptions();
+    }
+
     private void Start()
     {
-        volumeSlider.value = .4f;
-        SetVolume();
+        if (menuPrincipal != null && menuOpciones != null && SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            volumeSlider.value = .4f;
+            SetVolume();
+        }
     }
 
     public void PlayGame()
@@ -30,20 +38,65 @@ public class MenuPrincipal : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void OpenOptions()
+    public bool OpenOptions()
     {
-        menuPrincipal.SetActive(false);
-        menuOpciones.SetActive(true);
+        if (menuPrincipal != null && menuOpciones != null && SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            menuPrincipal.SetActive(false);
+            menuOpciones.SetActive(true);
+            return true;
+        }
+
+        if(menuGameplay != null && menuOpciones != null && SceneManager.GetActiveScene().name == "Level1")
+        {
+            menuGameplay.SetActive(false);
+            menuOpciones.SetActive(true);
+            Time.timeScale = 0;
+            return true;
+        }
+
+        return false;
     }
 
-    public void OpenMainMenu()
+    public bool ClosePause()
     {
-        menuOpciones.SetActive(false);
-        menuPrincipal.SetActive(true);
+        if (menuGameplay != null && menuOpciones != null && SceneManager.GetActiveScene().name == "Level1")
+        {
+
+            menuOpciones.SetActive(false); 
+            menuGameplay.SetActive(true);
+
+            Time.timeScale = 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool OpenMainMenu()
+    {
+        if(menuPrincipal != null && menuOpciones != null && SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            menuOpciones.SetActive(false);
+            menuPrincipal.SetActive(true);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void GotoMainMenu() {
+        SceneManager.LoadScene("MenuPrincipal");
     }
 
     public void SetVolume()
     {
         volumeMixer.SetFloat("MusicVol", Mathf.Log10(volumeSlider.value) * 20);
-    } 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
